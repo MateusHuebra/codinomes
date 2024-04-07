57cec603dfb3a31545913fe5466eb84bbe24f0b3
Mateus Huebra <mateus.huebra@scandiweb.com>
2024-04-07 11:15:20 -0300

    typo fixed

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/bot/listen', [Codinomes::class, 'listen']);
+Route::post('/bot/listen', [CodinomesController::class, 'listen']);
 
 /*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
