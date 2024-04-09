58443ff94dab33559a21959119fce52cd06faf12
Mateus Huebra <mateus.huebra@scandiweb.com>
2024-04-09 15:40:44 -0300

    minor fxi

--- app/Events/Language/Set.php
+++ app/Events/Language/Set.php
@@ -26,10 +26,12 @@ class Set implements Event {
             $data = CDM::toArray($update->getData());
             if($message->getChat()->getType()==='private') {
                 $user = User::find($update->getFrom()->getId());
+                $chatId = $user->id;
                 self::setUserOrChatLanguage($user, $data, $bot, $message);
                 
             } else if($message->getChat()->getType()==='supergroup') {
                 $chat = Chat::find($message->getChat()->getId());
+                $chatId = $chat->id;
                 if($data[CDM::FIRST_TIME] || $chat->isTgUserAdmin($update->getFrom(), $bot)) {
                     self::setUserOrChatLanguage($chat, $data, $bot, $message);
                 } else {
@@ -39,7 +41,7 @@ class Set implements Event {
             }
 
             if($data[CDM::FIRST_TIME]) {
-                $bot->sendMessage($chat->id, AppString::get('start.questions'));
+                $bot->sendMessage($chatId, AppString::get('start.questions'));
             }
 
         };
