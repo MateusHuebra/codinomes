3ef5c57618a1147f05757c6a6721388994a79474
Mateus Huebra <mateus.huebra@scandiweb.com>
2024-04-15 12:03:49 -0300

    history command works on private

--- app/Actions/Game/History.php
+++ app/Actions/Game/History.php
@@ -4,6 +4,7 @@ namespace App\Actions\Game;
 
 use App\Actions\Action;
 use App\Models\Game;
+use App\Models\User;
 use App\Services\AppString;
 use TelegramBot\Api\BotApi;
 
@@ -12,12 +13,24 @@ class History implements Action {
     public function run($update, BotApi $bot) : Void {
         $message = $update->getMessage();
         $chatId = $message->getChat()->getId();
-        $game = Game::where('chat_id', $chatId)->first();
-        if(!$game) {
-            return;
-        }
 
-        $bot->sendMessage($chatId, $game->history??AppString::get('error.no_history'), null, false, $message->getMessageId(), null, false, null, null, true);
+        if($message->getChat()->getType()==='private') {
+            $user = User::find($chatId);
+            if($user->game_id) {
+                $game = Game::find($user->game_id);
+            }
+            
+        } else {
+            $game = Game::where('chat_id', $chatId)->first();
+        }
+        
+        if($game) {
+            $text = $game->history??AppString::get('error.no_history');
+        } else {
+            $text = AppString::get('error.no_game');
+        }
+        
+        $bot->sendMessage($chatId, $text, null, false, $message->getMessageId(), null, false, null, null, true);
     }
 
 }
