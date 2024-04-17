3f058d2a10059d71ba9a59ed2689941d44bfdc8b
Mateus Huebra <mateus.huebra@scandiweb.com>
2024-04-17 12:16:54 -0300

    moved some functions to getColoredCards

--- app/Actions/Game/Start.php
+++ app/Actions/Game/Start.php
@@ -36,7 +36,6 @@ class Start implements Action {
         $agentsA = $game->users()->fromTeamRole('a', 'agent');
         $masterB = $game->users()->fromTeamRole('b', 'master');
         $agentsB = $game->users()->fromTeamRole('b', 'agent');
-
         if($masterA->count()==0 || $agentsA->count()==0 || $masterB->count()==0 || $agentsB->count()==0) {
             $bot->sendAlertOrMessage($update->getId(), $chatId, 'error.no_required_players');
             if(!env('APP_ENV')=='local') {
@@ -44,8 +43,7 @@ class Start implements Action {
             }
         }
 
-        $teams = ['a', 'b'];
-        $firstTeam = $teams[rand(0, 1)];
+        $firstTeam = rand(0, 1) ? 'a' : 'b';
         $game->updateStatus('master_'.$firstTeam);
 
         $pack = Pack::find(1);
@@ -53,7 +51,34 @@ class Start implements Action {
         $randomizedCards = $cards->random(25);
         $shuffledCards = $randomizedCards->toArray();
         shuffle($shuffledCards);
+        $shuffledCards = $this->getColoredCards($shuffledCards, $firstTeam);
 
+        foreach ($shuffledCards as $key => $card) {
+            $gameCard = new GameCard;
+            $gameCard->game_id = $game->id;
+            $gameCard->id = $key;
+            $gameCard->text = $card['text'];
+            $gameCard->team = $card['team']??'w';
+            $gameCard->revealed = false;
+            if(env('APP_ENV')=='local') {
+                $gameCard->revealed = rand(0,1)?true:false;
+            }
+            $gameCard->save();
+        }
+
+        $text = $update->getMessage()->getText();
+        preg_match('/^.*\R.*\R.*\R\R.*\R.*\R.*\R\R/', $text, $matches);
+        $text.= AppString::getParsed('game.started');
+        try {
+            $bot->editMessageText($chatId, $messageId, $text, 'MakedownV2');
+            $bot->answerCallbackQuery($updateId, AppString::get('settings.loading'));
+        } catch(Exception $e) {}
+
+        $caption = new Caption(AppString::get('game.started'), null, 50);
+        Table::send($game, $bot, $caption);
+    }
+
+    private function getColoredCards(array $shuffledCards, string $firstTeam) {
         $teams = [
             'a' => 8+($firstTeam=='a'?1:0),
             'b' => 8+($firstTeam=='b'?1:0),
@@ -82,29 +107,7 @@ class Start implements Action {
             }
         }
 
-        foreach ($shuffledCards as $key => $card) {
-            $gameCard = new GameCard;
-            $gameCard->game_id = $game->id;
-            $gameCard->id = $key;
-            $gameCard->text = $card['text'];
-            $gameCard->team = $card['team']??'w';
-            $gameCard->revealed = false;
-            if(env('APP_ENV')=='local') {
-                $gameCard->revealed = rand(0,1)?true:false;
-            }
-            $gameCard->save();
-        }
-
-        $text = $update->getMessage()->getText();
-        preg_match('/^.*\R.*\R.*\R\R.*\R.*\R.*\R\R/', $text, $matches);
-        $text.= AppString::getParsed('game.started');
-        try {
-            $bot->editMessageText($chatId, $messageId, $text, 'MakedownV2');
-            $bot->answerCallbackQuery($updateId, AppString::get('settings.loading'));
-        } catch(Exception $e) {}
-
-        $caption = new Caption(AppString::get('game.started'), null, 50);
-        Table::send($game, $bot, $caption);
+        return $shuffledCards;
     }
 
 }
