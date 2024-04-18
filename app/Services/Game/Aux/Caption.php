178935cf61ceb72fba8507836bea6d3450d2ff13
Mateus Huebra <mateus.huebra@scandiweb.com>
2024-04-18 10:28:01 -0300

    font size change

--- app/Services/Game/Aux/Caption.php
+++ app/Services/Game/Aux/Caption.php
@@ -8,7 +8,7 @@ class Caption {
     public $text;
     public $titleSize;
 
-    public function __construct(string $title, string $text = null, int $titleSize = 26) {
+    public function __construct(string $title, string $text = null, int $titleSize = 30) {
         $this->title = $title;
         $this->text = $text;
         $this->titleSize = $titleSize;
