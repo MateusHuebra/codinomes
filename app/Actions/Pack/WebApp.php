7def7493519856fc01ee2a70ff1042f3b7ff3b2f
Mateus Huebra <mateus.huebra@scandiweb.com>
2024-04-20 14:57:03 -0300

    fix url 3

--- app/Actions/Pack/WebApp.php
+++ app/Actions/Pack/WebApp.php
@@ -14,7 +14,7 @@ class WebApp implements Action {
             [
                 [
                     'text' => 'Abrir app',
-                    'web_app' => ['url' => 'https://www.mateuskian.shop/codinomes/test']
+                    'web_app' => ['url' => 'https://mateuskian.shop/codinomes/test']
                 ]
             ]
         ]);
