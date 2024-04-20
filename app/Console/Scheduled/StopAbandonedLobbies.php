503db60842b9925898f414a947e68337efdaf0b1
Mateus Huebra <mateus.huebra@scandiweb.com>
2024-04-19 22:01:42 -0300

    stop only "creating" lobbies

--- app/Console/Scheduled/StopAbandonedLobbies.php
+++ app/Console/Scheduled/StopAbandonedLobbies.php
@@ -16,6 +16,9 @@ class StopAbandonedLobbies {
         
         $games = Game::all();
         foreach ($games as $game) {
+            if($game->status != 'creating') {
+                continue;
+            }
             $time = strtotime($game->status_updated_at);
             if($now - $time >= 1200) {
                 try {
