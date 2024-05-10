66b1bb0da93f83729a331292691bc853b8abfe64
Mateus Huebra <mateus.huebra@scandiweb.com>
2024-05-10 13:21:41 -0300

    changed guess message

--- app/Actions/Game/ChosenGuess.php
+++ app/Actions/Game/ChosenGuess.php
@@ -58,7 +58,10 @@ class ChosenGuess implements Action {
             'name' => $user->name,
             'id' => $user->id
         ], $chatLanguage, true);
-        $text = $mention.' '.$emoji.' '.AppString::parseMarkdownV2($card->text);
+        $text = AppString::get('game.attempted', [
+            'user' => $mention,
+            'card' => $emoji.' '.AppString::parseMarkdownV2($card->text)
+        ], $chatLanguage);
         try {
             $bot->sendMessage($game->chat_id, $text, 'MarkdownV2', false, null, new ReplyKeyboardRemove);
         } catch(Exception $e) {}
