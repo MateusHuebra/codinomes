b1ad06d98a626cb422a4ae91a6e8af26a53a9ed0
Mateus Huebra <mateus.huebra@scandiweb.com>
2024-05-10 21:08:05 -0300

    minor change

--- app/Actions/Game/Stop.php
+++ app/Actions/Game/Stop.php
@@ -6,7 +6,6 @@ use App\Actions\Action;
 use App\Adapters\UpdateTypes\Update;
 use TelegramBot\Api\BotApi;
 use App\Services\AppString;
-use TelegramBot\Api\Types\ReplyKeyboardRemove;
 
 class Stop implements Action {
 
@@ -25,7 +24,7 @@ class Stop implements Action {
         
         $game->stop($bot, true);
 
-        $bot->sendMessage($chat->id, AppString::get('game.stopped'), null, false, null, new ReplyKeyboardRemove);
+        $bot->sendMessage($chat->id, AppString::get('game.stopped'));
     }
 
 }
