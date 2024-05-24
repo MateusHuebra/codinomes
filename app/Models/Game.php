f4f87962426790e553ae01d0c0c658e1911e4ac3
Mateus Huebra <mateus.huebra@scandiweb.com>
2024-05-24 18:56:15 -0300

    fixed no hint string issue 2

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -74,10 +74,7 @@ class Game extends Model
 
     public function getLastHint() {
         if(!$this->lastHint) {
-            if(preg_match('/\*.+ (?<hint>SEM DICA ∞+)\*(\R>.+)*$/', $this->history, $matches)) {
-                $this->lastHint = $matches['hint'];
-            }
-            preg_match('/\*.+ (?<hint>[A-ZÁÀÂÃÉÈÊÍÏÓÔÕÖÚÇÑ ]{1,16} [0-9∞]+)\*(\R>.+)*$/', $this->history, $matches);
+            preg_match('/\*.. (?<hint>[\s\S]{1,16} [0-9∞]+)\*(\R>.+)*$/', $this->history, $matches);
             $this->lastHint = $matches['hint'];
         }
         return $this->lastHint;
