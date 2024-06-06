da3a55481d1edd1ebb07a12ebcd3f1d2e04fbc2a
Mateus Huebra <mateus.huebra@scandiweb.com>
2024-06-06 18:40:18 -0300

    added suggestion do set default color

--- app/Actions/Game/Color.php
+++ app/Actions/Game/Color.php
@@ -4,6 +4,7 @@ namespace App\Actions\Game;
 
 use App\Actions\Action;
 use App\Adapters\UpdateTypes\Update;
+use App\Services\AppString;
 use App\Services\Game\Menu;
 use TelegramBot\Api\BotApi;
 use App\Services\CallbackDataManager as CDM;
@@ -44,6 +45,10 @@ class Color implements Action {
         $game->save();
         
         Menu::send($game, $bot);
+
+        if($user->default_color == null && rand(1, 10) == 1) {
+            $bot->sendMessage($update->getChatId(), AppString::get('color.suggestion'));
+        }
     }
 
 }
