dfd3af7f9196f1c224d6c2bb62c6d2c56d6c146f
Mateus Huebra <mateus.huebra@scandiweb.com>
2024-06-27 19:50:55 -0300

    PrivateCoopMessage

---
+++ app/Actions/PrivateCoopMessage.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Actions;
+
+use App\Adapters\UpdateTypes\Update;
+use App\Models\Game;
+use App\Models\GlobalSettings;
+use TelegramBot\Api\BotApi;
+
+class PrivateCoopMessage implements Action {
+
+    public function run(Update $update, BotApi $bot) : Void {
+        if(!$user = $update->findUser()) {
+            return;
+        }
+        if(!$game = $user->currentGame()) {
+            return;
+        }
+        if($game->mode != 'coop') {
+            return;
+        }
+        if($game->users()->count() < 2) {
+            return;
+        }
+
+        $recipient = $game->users()
+                            ->where('id', '!=', $user->id)
+                            ->first();
+        
+        if($update->getMessageText() || $update->getMessage()->getSticker() || $update->getMessage()->getVoice()) {
+            $bot->forwardMessage($recipient->id, $update->getChatId(), $update->getMessageId());
+        }
+    }
+
+}
